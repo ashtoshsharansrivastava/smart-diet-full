a9128625667fa90680070d881de93e26275fe054
Ashutosh sharan srivastava <ashutoshsharansrivastava@gmail.com>
2025-12-21 10:38:44 +0530

    i have added the required files for the dietitian

--- server/server.js
+++ server/server.js
@@ -47,9 +47,7 @@ app.use(cors({
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/diet-plans', require('./routes/dietPlans'));
 app.use('/api/recipes', require('./routes/recipes'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/diet-plans', require('./routes/dietPlans'));
-app.use('/api/recipes', require('./routes/recipes'));
+
 app.use('/api/dietitians', require('./routes/dietitians'));
 
 // ------------------
