23d30e99c953526d03e90f56ad09e80295f43e53
Ashutosh sharan srivastava <ashutoshsharansrivastava@gmail.com>
2025-12-21 10:50:01 +0530

    i have added the required files for the dietitian

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- THIS LINE IS CRITICAL
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // 👈 1. Import this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 👇 2. Wrap App inside BrowserRouter */}
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
