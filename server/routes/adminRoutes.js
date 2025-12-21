f3da2c9df55c93b43cdb1c6dc747841da42cfe32
Ashutosh sharan srivastava <ashutoshsharansrivastava@gmail.com>
2025-12-21 12:28:20 +0530

    i have renamed the authMiddlewares

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../middleware/authmiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 const User = require('../models/User');
 
 // @desc    Get all users who have applied to be dietitians
